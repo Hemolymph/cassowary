a708fac6ed3040ea86a785e8664fbc637006c941
Katie Ampersand <katie-and@ampersandia.net>
2025-05-30 01:27:28 -0500

    This print was annoying me

--- server/src/main.rs
+++ server/src/main.rs
@@ -402,7 +402,6 @@ async fn room_task(
                         };
 
                         game.state.push_card(card, to, local_side);
-                        println!("{:#?}", game.state);
 
                         if game.state.get_state(local_side).searching.is_some() {
                             to_players
